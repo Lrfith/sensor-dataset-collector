5377c2508d1a14ae696659289047bf5247090a98
AS <98580462+Lrfith@users.noreply.github.com>
2025-04-29 08:19:41 +0700

    Use Foever run

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-const { server } = require('./service/server');
+const { model_trainer } = require('./service/model_trainer.js');
+const { server } = require('./service/server.js');
+const { syncFirebaseToMongo } = require('./service/syncFirebaseToMongo.js');
 
 async function startApp() {
     try {
-        server();
+        // เรียกใช้ฟังก์ชันแต่ละตัวที่เป็น async
+        await model_trainer();
+        console.log('Model Trainer completed');
+        
+        await server();
         console.log('Server started');
+        
+        await syncFirebaseToMongo();
+        console.log('Firebase to Mongo sync completed');
     } catch (error) {
         console.error('Error during app startup:', error);
-        process.exit(1);
+        process.exit(1);  // Exit the process with a failure code
     }
 }
 
