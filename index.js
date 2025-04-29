f462b4aeaae07997aea0742cd4a6d30ecd234bbf
AS <98580462+Lrfith@users.noreply.github.com>
2025-04-29 08:04:05 +0700

    Only server

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-const { model_trainer } = require('./service/model_trainer.js');
 const { server } = require('./service/server.js');
-const { syncFirebaseToMongo } = require('./service/syncFirebaseToMongo.js');
 
 async function startApp() {
     try {
-        // เรียกใช้ฟังก์ชันแต่ละตัวที่เป็น async
-        await model_trainer();
-        console.log('Model Trainer completed');
-        
         server();
         console.log('Server started');
-        
-        await syncFirebaseToMongo();
-        console.log('Firebase to Mongo sync completed');
     } catch (error) {
         console.error('Error during app startup:', error);
-        process.exit(1);  // Exit the process with a failure code
+        process.exit(1);
     }
 }
 
